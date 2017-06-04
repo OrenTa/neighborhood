3c0d2e13de47e09c0d8e3af51fe77319605fb41d
Oren Tayar <oren.tayar@gmail.com>
2017-06-05 00:03:04 +0300

    spaces and tabs ...

--- js/app.js
+++ js/app.js
@@ -14,31 +14,31 @@ loc = {lat: 13.750, lng: 100.503};
 // Model - holds the data 
 // apart of what is shown here the model is also enriched later with wikipedia image urls
 var locations = [
-    {name: 'Wat Ratchabophit', loc: {lat: 13.750122, lng: 100.499171},
-        show: ko.observable(true), id: '0', wikiurl: ko.observable('')},
-    {name: 'Giant Swing', loc: {lat: 13.752040, lng: 100.499600},
-        show: ko.observable(true), id: '1', wikiurl: ko.observable('')},
-    {name: 'Wat Thep Sirin Thrawat', loc: {lat: 13.746287, lng: 100.509814},
-        show: ko.observable(true), id: '2', wikiurl: ko.observable('')},
-    {name: 'Bobe Market Bridge', loc: {lat: 13.753040, lng: 100.518741},
-        show: ko.observable(true), id: '3', wikiurl: ko.observable('')},
-    {name: 'Wat Mangkon Kamalawat', loc: {lat: 13.7436, lng: 100.511874},
-        show: ko.observable(true), id: '4', wikiurl: ko.observable('')},
-    {name: 'Wat Bowonniwet Vihara', loc: {lat: 13.75445, lng: 100.505351},
-        show: ko.observable(true), id: '5', wikiurl: ko.observable('')},
-    {name: 'Wat saket', loc: {lat: 13.760294, lng: 100.499861},
-        show: ko.observable(true), id: '6', wikiurl: ko.observable('')},
+	{name: 'Wat Ratchabophit', loc: {lat: 13.750122, lng: 100.499171},
+		show: ko.observable(true), id: '0', wikiurl: ko.observable('')},
+	{name: 'Giant Swing', loc: {lat: 13.752040, lng: 100.499600},
+		show: ko.observable(true), id: '1', wikiurl: ko.observable('')},
+	{name: 'Wat Thep Sirin Thrawat', loc: {lat: 13.746287, lng: 100.509814},
+		show: ko.observable(true), id: '2', wikiurl: ko.observable('')},
+	{name: 'Bobe Market Bridge', loc: {lat: 13.753040, lng: 100.518741},
+		show: ko.observable(true), id: '3', wikiurl: ko.observable('')},
+	{name: 'Wat Mangkon Kamalawat', loc: {lat: 13.7436, lng: 100.511874},
+		show: ko.observable(true), id: '4', wikiurl: ko.observable('')},
+	{name: 'Wat Bowonniwet Vihara', loc: {lat: 13.75445, lng: 100.505351},
+		show: ko.observable(true), id: '5', wikiurl: ko.observable('')},
+	{name: 'Wat saket', loc: {lat: 13.760294, lng: 100.499861},
+		show: ko.observable(true), id: '6', wikiurl: ko.observable('')},
 	{name: 'Thammasat University', loc: {lat: 13.759400, lng: 100.491093},
-        show: ko.observable(true), id: '7', wikiurl: ko.observable('')}
+		show: ko.observable(true), id: '7', wikiurl: ko.observable('')}
 ];
 
 
 // Google Maps init function
 function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: loc,
-        zoom: 15
-    });
+	map = new google.maps.Map(document.getElementById('map'), {
+		center: loc,
+		zoom: 15
+	});
 	console.log(google);
 	ko.applyBindings(new ViewModel());
 }
@@ -46,10 +46,10 @@ function initMap() {
 // KO viewModel
 // Adds to the map markers and infowindows and attaches these info windows to markers
 var ViewModel = function () {
-    places = ko.observableArray(locations);
-    this.searchTerm = ko.observable('');//this is the searchTerm
+	places = ko.observableArray(locations);
+	this.searchTerm = ko.observable('');//this is the searchTerm
     
-    var marker, infowindow;
+	var marker, infowindow;
 	
 	// Fetches img url from wikipedia based on the locations[].name parameter
 	// this function is part of the contol(modelview) as it fetches info from wikipedia and updates the // model (locations) with it.
@@ -95,13 +95,13 @@ var ViewModel = function () {
 					} else {
 						element.imageurl = PROBLEM_IMAGE;
 					}
-                    return element;
-                });
-              });
-	      }); // end of forEach
+					return element;
+				});
+			});
+		}); // end of forEach
 				
-	   } // end of getDatafunction 
-        	
+	} // end of getDatafunction 
+ 	
 	getData();
 	
 	var temploc;
@@ -112,10 +112,10 @@ var ViewModel = function () {
 	var setMarkers = function() {	
 		
 		 infowindow = new google.maps.InfoWindow({
-						  content: '<div>' + 'name goes here' + '</div>' + 
-						  '<div><img src="' + 'image url will go here' + '" style="width:100px" alt="wiki image"></div>',
-						  maxWidth: '200'
-					   });
+			 content: '<div>' + 'name goes here' + '</div>' + 
+			 '<div><img src="' + 'image url will go here' + '" style="width:100px" alt="wiki image"></div>',
+			 maxWidth: '200'
+		 });
 		
 		for (i = 0; i < locations.length; i++) {
 			temploc = locations[i];
@@ -131,66 +131,66 @@ var ViewModel = function () {
 	
 	// this is the click function for the markers - it basically sets the infowindow and opens it
 	var infoShow = function (markercopy, icopy ) {
-                            return function () {
-                                var tempcont = '<div>' + locations[icopy].name + '</div>' + 
-									'<div><img src="' + locations[icopy].imageurl + '" style="width:100px" alt="wiki image"></div>';
-								infowindow.setContent(tempcont);
-								infowindow.open(map, markercopy);
-                                markercopy.setAnimation(google.maps.Animation.BOUNCE);
-                                setTimeout(function () {
-                                    markercopy.setAnimation(null);
-                                }, 1200);
-							};
+		return function () {
+			var tempcont = '<div>' + locations[icopy].name + '</div>' + 
+				'<div><img src="' + locations[icopy].imageurl + '" style="width:100px" alt="wiki image"></div>';
+			infowindow.setContent(tempcont);
+			infowindow.open(map, markercopy);
+			markercopy.setAnimation(google.maps.Animation.BOUNCE);
+			setTimeout(function () {
+				markercopy.setAnimation(null);
+			}, 1200);
+		};
 	};
 	
 	setMarkers();
-                 
-    // this function updates the visible markers on the map
+
+	// this function updates the visible markers on the map
     // according to the show parameter in the array (true/false)
-    function updateMap() {
-        //marker.setVisible(false);
-        for (i = 0; i < markers.length; i++) {
+	function updateMap() {
+		//marker.setVisible(false);
+		for (i = 0; i < markers.length; i++) {
 			markers[i].setVisible(places()[i].show());
-        }
-    }
-    
+		}
+	}
+
 	// animates the marker and shows its infowindow upon click in the list
-    this.locationClicked = function () {
-        var tempmarker = markers[parseInt($(this).attr('id'))];
+	this.locationClicked = function () {
+		var tempmarker = markers[parseInt($(this).attr('id'))];
 		infoShow(tempmarker,parseInt($(this).attr('id')))();	
 		$(this).parent().find('div').css('background-color', 'white');
-        $(this).css('background-color', 'orange');
-    };
+		$(this).css('background-color', 'orange');
+	};
     
-    // this function is run for filtering the list
-    // it is binded to the search input box in the html
-    // after filtering it calls the updatemap function also defined in the ViewModel
-    this.listFilter = function () {
-        for (i = 0; i < places().length; i++) {
-            if (places()[i].name.toLowerCase().includes(this.searchTerm().toLowerCase())) {
-                places()[i].show(true);
-            } else {
-                places()[i].show(false);
-            }
-        }
-        updateMap();
-    };//end of filterfunction.
-    
-    this.listFilter(); //run this one time on initialization.
+	// this function is run for filtering the list
+	// it is binded to the search input box in the html
+	// after filtering it calls the updatemap function also defined in the ViewModel
+	this.listFilter = function () {
+		for (i = 0; i < places().length; i++) {
+			if (places()[i].name.toLowerCase().includes(this.searchTerm().toLowerCase())) {
+				places()[i].show(true);
+			} else {
+				places()[i].show(false);
+			}
+		}
+		updateMap();
+	};//end of filterfunction.
+
+	this.listFilter(); //run this one time on initialization.
 }; // end of viewmode function
 
 
 // Shows or hides the side navigation in 'mobile view' 
 // the binding is done with knockout in the HTML itself to the 'hamburger'
 function showNav() {
-    if (document.getElementById('list').style.width === '0px') {
-        document.getElementById('list').style.width = '250px';
-        document.getElementById('list').style.height = 'auto';
-    } else {
-        //console.log('width is not 0px but ' + document.getElementById('list').style.width)
-        document.getElementById('list').style.width = '0px';
-        document.getElementById('list').style.height = 'auto';
-    }
+	if (document.getElementById('list').style.width === '0px') {
+		document.getElementById('list').style.width = '250px';
+		document.getElementById('list').style.height = 'auto';
+	} else {
+		//console.log('width is not 0px but ' + document.getElementById('list').style.width)
+		document.getElementById('list').style.width = '0px';
+		document.getElementById('list').style.height = 'auto';
+	}
 }
 
 function googleError() {
